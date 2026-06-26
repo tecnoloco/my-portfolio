0ff10f512765b96dbe1ef428d33d4c8bd10d47c3
Eduardo Espinosa <edan.espinosa@gmail.com>
2026-06-26 12:37:43 -0600

    chore: lint

--- app/components/sections/ProjectsGrid.tsx
+++ app/components/sections/ProjectsGrid.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
